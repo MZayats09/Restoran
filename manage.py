7ef7660e090c59779a7b5cec48825d71b3ec1145
mrslvzyts <mudludalaad@gmail.com>
2026-09-20 13:10:15 +0300

    Add files via upload

---
+++ manage.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Не вдалося імпортувати Django. Переконайся, що він встановлений і "
+            "доступний у змінній середовища PYTHONPATH. Ти активував(ла) віртуальне середовище?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
