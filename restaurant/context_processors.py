3a0440accf2307b4fecee7a79be3564d4514b751
mrslvzyts <mudludalaad@gmail.com>
2026-09-20 12:50:12 +0300

    Add files via upload

--- restaurant/context_processors.py
+++ restaurant/context_processors.py
@@ -1,8 +1,13 @@
 from .cart import Cart
+from .models import Order
 
 
 def cart_summary(request):
     cart = Cart(request)
+    unread = 0
+    if request.user.is_authenticated:
+        unread = Order.objects.filter(user=request.user, status='ready', ready_notified=False).count()
     return {
         'cart_count': cart.total_quantity(),
+        'unread_notifications': unread,
     }
